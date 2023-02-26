cf2956e7c4590d0338e8719409225b67fb3e8c7b
Webert Dias <wdwebert@gmail.com>
2023-02-26 16:10:30 -0300

    Iniciando js

--- script.js
+++ script.js
@@ -1,3 +1,46 @@
 
+            const entrada = document.querySelector('#entrada')
+            const resultado = document.querySelector('#resultado')
 
+            function cincoPorCento(){
+                event.preventDefault();
+                const porcentagem = parseFloat(entrada.value) / 100 *5;
+                resultado.innerHTML += parseInt(entrada.value) + porcentagem;
+            }
+
+            function dezPorCento() {
+                event.preventDefault();
+                console.log(resultado.innerHTML)
+                const porcentagem = (parseInt(entrada.value) / 100) * 10;
+                resultado.innerHTML += parseInt(entrada.value) + porcentagem;
+               
+            }
+            function quinzePorCento() {
+                event.preventDefault();
+                console.log(resultado.innerHTML)
+                const porcentagem = (parseInt(entrada.value) / 100) * 15;
+                resultado.innerHTML += parseInt(entrada.value) + porcentagem;
+               
+            }
+            function vinteCincoPorCento() {
+                event.preventDefault();
+                console.log(resultado.innerHTML)
+                const porcentagem = (parseInt(entrada.value) / 100) * 25;
+                resultado.innerHTML += parseInt(entrada.value) + porcentagem;
+               
+            }
+            function cinquentaPorCento() {
+                event.preventDefault();
+                console.log(resultado.innerHTML)
+                const porcentagem = (parseInt(entrada.value) / 100) * 50;
+                resultado.innerHTML += parseInt(entrada.value) + porcentagem;
+               
+            }
+            
+            
+           
+           
+           
+            
+           
 
